241fcdc1bd91d65b52409c10cb93a7e610945104
Hafiz Muhammad Furqan <hafizm.furqan123@gmail.com>
2025-01-01 21:57:21 +0500

    main app basic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,13 +2,15 @@ import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import UserRole from "./pages/UserRole";
+import MainApp from "./pages/MainApp";
 
 const App = () => {
   return (
     // <Home />
     // <Signup />
     // <Signin />
-    <UserRole />
+    // <UserRole />
+    <MainApp />
   );
 };
 
